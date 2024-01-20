01c81cdf1fc4bdb3af0b6f2ad379d1c1ab88ac3c
dand <79340018+dZyad@users.noreply.github.com>
2024-01-20 19:42:54 -0300

    Add Calculator object and basic operations

---
+++ calculator.js
@@ -0,0 +1,21 @@
+let calculator = {
+    display: 0,
+    value: 0,
+    operator: '+',
+}
+
+function add( first, second ) {
+    return first + second;
+}
+
+function subtract( first, second ) {
+    return first - second;
+}
+
+function multiply( first, second ) {
+    return first * second;
+}
+
+function divide ( first, second ) {
+    return first / second;
+}
